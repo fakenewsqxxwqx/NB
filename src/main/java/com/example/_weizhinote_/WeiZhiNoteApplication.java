dd65c9662ff20cec5c07fce7facb9880315ba5a9
YU YANG <yy_17633796552@outlook.com>
2023-06-27 11:08:03 +0800

    pom.xml配置

---
+++ src/main/java/com/example/_weizhinote_/WeiZhiNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example._weizhinote_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiZhiNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeiZhiNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/_weizhinote_/WeiZhiNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._weizhinote_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiZhiNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
