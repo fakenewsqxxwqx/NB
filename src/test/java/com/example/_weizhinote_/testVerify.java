803107485f15a758c94c40eb2cb86169bb6e1f4c
YU YANG <yy_17633796552@outlook.com>
2023-07-02 21:43:19 +0800

    测试

---
+++ src/test/java/com/example/_weizhinote_/testVerify.java
@@ -0,0 +1,16 @@
+package com.example._weizhinote_;
+
+import com.example._weizhinote_.utils.verity;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class testVerify {
+
+    @Test
+    public void testVerify() throws IOException {
+        //verity.verfityCode();
+    }
+}
