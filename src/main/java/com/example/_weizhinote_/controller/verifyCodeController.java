f156f77c8a6c97a70a455be998da7e8581aac107
yjx <1060710976@qq.com>
2023-07-03 09:31:44 +0800

    图形验证码

--- src/main/java/com/example/_weizhinote_/controller/verifyCodeController.java
+++ src/main/java/com/example/_weizhinote_/controller/verifyCodeController.java
@@ -1,5 +1,6 @@
 package com.example._weizhinote_.controller;
 
+import com.example._weizhinote_.utils.verifyCode;
 import com.wf.captcha.SpecCaptcha;
 import com.wf.captcha.base.Captcha;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,37 +17,15 @@ public class verifyCodeController {
 
     //生成验证码
     @GetMapping("/kaptcha")
-    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        httpServletResponse.setHeader("Cache-Control", "no-store");
-        httpServletResponse.setHeader("Pragma", "no-cache");
-        httpServletResponse.setDateHeader("Expires", 0);
-        httpServletResponse.setContentType("image/gif");
-
-        //生成验证码对象,三个参数分别是宽、高、位数
-        SpecCaptcha captcha = new SpecCaptcha(130, 48, 5);
-        //设置验证码的字符类型为数字和字母混合
-        captcha.setCharType(Captcha.TYPE_DEFAULT);
-        // 设置内置字体
-        captcha.setCharType(Captcha.FONT_1);
-        //验证码存入session
-        httpServletRequest.getSession().setAttribute("verifyCode", captcha.text().toLowerCase());
-        //输出图片流
-        captcha.out(httpServletResponse.getOutputStream());
+    public void generateVerifyCode(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        verifyCode.defaultKaptcha(httpServletRequest, httpServletResponse);
     }
+
 
     //校验验证码
     @GetMapping("/verify")
     public String verify(@RequestBody Map<String, String> requestBody, HttpSession session){
-        String code=requestBody.get("code");
-
-        if (!StringUtils.hasLength(code)){
-            return "验证码不能为空";
-        }
-        String kaptchaCode = session.getAttribute("verifyCode")+"";
-        if (!StringUtils.hasLength(kaptchaCode)||!code.toLowerCase().equals(kaptchaCode)){
-            return "验证码错误";
-        }
-        return "验证成功";
+        return verifyCode.verify(requestBody, session);
     }
 
 }
